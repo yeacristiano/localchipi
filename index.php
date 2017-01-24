0a5576927eb23cb163cf58af2f5f34bc721b685d
yeacristiano <yael_chelo@hotmail.com>
2017-01-24 13:30:19 -0600

    nuevos cambios

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+
+function saludo ( $nombre ){
+	
+	$maquina2="macbook pro";
+	return "hola como estas " . $nombre . " te saluda " . $maquina2;
+}
+
+
+require "vistas/menu.php";
---
+++ vistas/menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ejercicios</title>
+</head>
+<body>
+<h1>Esta es mi pagina de ejercicios</h1>
+
+<UL>
+<LI> Primera voz del menú
+<LI> Segunda voz del menú
+<LI> Tercera voz del menú
+</UL>
+
+</body>
+</html>
